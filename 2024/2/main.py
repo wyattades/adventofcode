c9e9bb8b4a9d99c1f8b972c2d9e5b058a06b710d
Wyatt Ades <me@wyattades.com>
2024-12-07 10:54:23 -0500

    fetch prompts

---
+++ 2024/2/main.py
@@ -0,0 +1,23 @@
+
+class Utils:
+  @staticmethod
+  def as_lines(raw_input):
+    return raw_input.strip().split("\n")
+
+def level_1(raw_input):
+  lines = Utils.as_lines(raw_input)
+
+  raise "Failed to calculate answer!"
+
+  answer = 0
+
+  answer
+
+def level_2(raw_input):
+  lines = Utils.as_lines(raw_input)
+
+  raise "Failed to calculate answer!"
+
+  answer = 0
+
+  answer
---
+++ 2024/4/main.rb
@@ -0,0 +1,19 @@
+def level_1(raw_input)
+  lines = Utils.as_lines(raw_input)
+
+  raise "Failed to calculate answer!"
+
+  answer = 0
+
+  answer
+end
+
+def level_2(raw_input)
+  lines = Utils.as_lines(raw_input)
+
+  raise "Failed to calculate answer!"
+
+  answer = 0
+
+  answer
+end
